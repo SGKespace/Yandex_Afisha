a7bf6807fa529a2c731a3fec390b0dd78f8f3c9f
SGK <55636018+SGKespace@users.noreply.github.com>
2023-09-10 10:38:51 +0400

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,2 +1,14 @@
 from django.db import models
-print("hello!")
+
+class Place(models.Model):
+
+    title = models.CharField('Название места', max_length=100)
+    description_short = models.CharField('Короткое описание', max_length=100)
+    description_long = models.TextField("Подробное описание")
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+
+
+    def __str__(self):
+
+        return self.title
