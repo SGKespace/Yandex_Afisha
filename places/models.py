7e344f8f205f841d6a2596c2a5c4b6c353c93b8f
SGK <55636018+SGKespace@users.noreply.github.com>
2023-09-18 22:42:23 +0400

    проиндексировал

--- places/models.py
+++ places/models.py
@@ -19,7 +19,7 @@ class Image(models.Model):
     image = models.ImageField('Изображение', upload_to='')
     place = models.ForeignKey(
         'Place', verbose_name='Место', related_name='images', on_delete=models.CASCADE)
-    position = models.PositiveIntegerField('Позиция', default=0, blank=True)
+    position = models.PositiveIntegerField('Позиция', default=0, blank=True, db_index=True)
 
     @property
     def image_preview(self):
