c0555d128d91b17d6657638c8a2272a36a20dc44
SGK <55636018+SGKespace@users.noreply.github.com>
2023-09-21 15:05:34 +0400

    ограничил ширину фото

--- places/models.py
+++ places/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from tinymce.models import HTMLField
+from django.utils.html import format_html
 
 
 class Place(models.Model):
-    title = models.CharField('Название места', unique=True, max_length=100)
+    title = models.CharField('Название места', max_length=100)
     short_description = models.TextField('Короткое описание', blank=True)
     long_description = HTMLField("Подробное описание", blank=True)
     lat = models.FloatField('Широта')
@@ -16,16 +16,16 @@ class Place(models.Model):
 
 
 class Image(models.Model):
+    image = models.ImageField('Изображение', upload_to='')
     place = models.ForeignKey(
         'Place', verbose_name='Место', related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField('Изображение', upload_to='')
-    position = models.PositiveIntegerField('Позиция', default=0, blank=True, db_index=True)
+    position = models.PositiveIntegerField('Позиция', default=0)
 
     @property
-    def preview_image(self):
+    def image_preview(self):
         if self.image:
             return format_html(
-                mark_safe('<img src="{}" style="max-height:300px">'),
+                mark_safe('<img src="{}" style="max-height:300px, max-height: 300px">'),
                 self.image.url,
                 )
         return ""
